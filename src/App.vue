3d1308408ac698b5f097f6c8840043d530f5053d
EzefcDev <ezeferrauti@gmail.com>
2023-07-30 17:32:25 -0300

    timeline component created

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <v-app>
     <v-container>
       <CardReservation @click="save" :reservations="reservations" />
-
+      <Timeline :items="reservations"/>
       <TableReservation :reservations="reservations" />
     </v-container>
   </v-app>
@@ -11,12 +11,14 @@
 <script>
 import TableReservation from "./components/TableReservation/TableReservation.vue";
 import CardReservation from "./components/CardReservation/CardReservation.vue";
+import Timeline from "./components/Timeline/Timeline.vue";
 
 export default {
   name: "App",
   components: {
     TableReservation,
     CardReservation,
+    Timeline
   },
   data() {
     return {
@@ -26,7 +28,19 @@ export default {
   methods: {
     save(info) {
       this.reservations.push(info)
+      this.reservations.sort((a,b)=>{
+        const dateA = this.parseObject(a)
+        const dateB = this.parseObject(b)
+        return dateA - dateB
+      })
       localStorage.setItem('reservations', JSON.stringify(this.reservations))
+    },
+    parseObject(object){
+      const dateParts = object.date.split("-");
+      const timeParts = object.hours.split(" ")[1].split(":");
+      const [year, month, day] = dateParts;
+      const [hour, minute] = timeParts;
+      return new Date(year, month - 1, day, hour, minute);
     },
     getReservations(){
         this.reservations = JSON.parse(localStorage.getItem("reservations")) ?? []
--- src/components/Timeline/Timeline.vue
+++ src/components/Timeline/Timeline.vue
@@ -1,25 +1,44 @@
 <template>
-  <v-timeline>
-    <v-timeline-item>timeline item</v-timeline-item>
-    <v-timeline-item class="text-right">
-      timeline item
+  <v-timeline class="horizontal" align-top dense>
+    <v-timeline-item 
+      v-for="(item, i) in items"
+      :key="i"
+     fill-dot
+     small
+     >
+     {{ item.date }} - {{ item.name }}
     </v-timeline-item>
-    <v-timeline-item>timeline item</v-timeline-item>
   </v-timeline>
 </template>
 
 <script>
-   export default{
-     data(){
-        return{
-        }
-    },
-    computed:{
-        url(){
-            return './timeline.html';
-        }
+    export default{
+        props:{
+          items:{
+            type: Array,
+          },
+        },
+        data(){
+            return{
+            }
+        },
+        computed:{
+            url(){
+                return './timeline.html';
+            }
+        },
     }
-   }
 </script>
 
+<style scoped>
+.horizontal{
+  display: grid;          
+  grid-template-columns: repeat(auto-fit, minmax(20px, 1fr));
+  gap: 5px;  
+}
+.v-timeline::before {
+  content: none;
+}
+
+</style>
 
